4e3a5f45fcd28a3e35a5d383c58b79eb1bb5d31a
JuanTiradov <jtiradoverbel06@correo.unicordoba.edu.co>
2024-04-09 15:57:32 -0500

    crud generado

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { filesController } from './files.controller';
 import { filesService } from './files.service';
+import { FilesModule } from './files/files.module';
 
 @Module({
   controllers: [filesController],
-  providers: [filesService]
+  providers: [filesService],
+  imports: [FilesModule]
 })
 export class filesModule {}
